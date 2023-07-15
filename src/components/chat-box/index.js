de88de86426b49efd96765c3701a973a30691087
akhileshkr236 <akhileshkr236@gmail.com>
2023-07-15 13:07:20 +0530

    changes

--- src/components/chat-box/index.js
+++ src/components/chat-box/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import SimpleBar from "simplebar-react";
 import { useUsers } from "../../contexts/UsersContext";
@@ -6,10 +6,15 @@ import { useUsers } from "../../contexts/UsersContext";
 const Chatbox = () => {
   const [isChatMenuOpen, setIsChatMenuOpen] = useState(false);
   const [isChattingOpen, setIsChattingOpen] = useState(false);
+  const [singleChatDetail, setSingleChatDetail] = useState({});
 
   const users = useUsers();
 
-  console.log(users);
+  useEffect(() => {
+    for (let i = 0; i < users.length; i++) {
+      users[i].isActive = Math.random() < 0.5;
+    }
+  }, []);
 
   const toggleChatMenu = () => {
     setIsChatMenuOpen(!isChatMenuOpen);
@@ -21,6 +26,11 @@ const Chatbox = () => {
 
   const closeChattingbox = () => {
     setIsChattingOpen(false);
+  };
+
+  const openSingleChat = (item) => {
+    setIsChattingOpen(true);
+    setSingleChatDetail(item);
   };
 
   return (
@@ -48,8 +58,17 @@ const Chatbox = () => {
           >
             <ul className="chat-list">
               {users.map((item) => (
-                <li key={item.id}>
-                  <Link to="#" className="chat-list-item text-truncate">
+                <li
+                  key={item.id}
+                  type="button"
+                  onClick={() => openSingleChat(item)}
+                >
+                  <Link
+                    to="#"
+                    className={`chat-list-item text-truncate ${
+                      item?.isActive ? "active" : ""
+                    }`}
+                  >
                     <img src={item.profilepicture} alt="..." />
                     {item.name}
                   </Link>
@@ -59,58 +78,60 @@ const Chatbox = () => {
           </SimpleBar>
         </div>
       </div>
-      <div className="chat-msg-box chatting-msg-box">
-        <button
-          className="btn chat-menu-toggle text-truncate"
-          onClick={toggleChattingBox}
-        >
-          <img
-            src="https://panorbit.in/wp-content/uploads/2019/hotlink-ok/1003.jpeg"
-            className="profile-icon"
-          />
-          NAme asdf
-          <span
-            className={`close-icon ${
-              isChattingOpen ? "icon-chevron-down" : "icon-chevron-up"
-            }`}
-          ></span>
-          <button className="btn icon-btn">
-            <span className="icon-x"></span>
-          </button>
-        </button>
-        <div
-          className={`chat-menu-content ${
-            isChattingOpen ? "d-block" : "d-none"
-          }`}
-        >
-          <SimpleBar
-            forceVisible="y"
-            autoHide={false}
-            className="custom-scrollbar"
-            style={{ maxHeight: "260px" }}
+      {isChattingOpen && (
+        <div className="chat-msg-box chatting-msg-box">
+          <button
+            className="btn chat-menu-toggle text-truncate"
+            onClick={closeChattingbox}
           >
-            <div>
-              <div className="recieved msg-chat">
-                <p>Loream ipsum dolor sldkfj sdklfj </p>
-                <p>Loream ipsum dolor sldkfj sdklfj </p>
-              </div>
-              <div className="timestamp">
-                <span>3:16PM</span>
-              </div>
-              <div className="send msg-chat">
-                <p>Loream ipsum dolor sldkfj sdklfj </p>
-                <p>Loream ipsum dolor sldkfj sdklfj </p>
-              </div>
-            </div>
-          </SimpleBar>
-          <form className="input-msg-box">
-            <input type="text" />
+            <img
+              src={singleChatDetail?.profilepicture}
+              className="profile-icon"
+            />
+            {singleChatDetail?.name}
+            <span
+              className={`close-icon ${
+                isChattingOpen ? "icon-chevron-down" : "icon-chevron-up"
+              }`}
+            ></span>
             <button className="btn icon-btn">
-              <span className="icon-send"></span>
+              <span className="icon-x"></span>
             </button>
-          </form>
+          </button>
+          <div
+            className={`chat-menu-content ${
+              isChattingOpen ? "d-block" : "d-none"
+            }`}
+          >
+            <SimpleBar
+              forceVisible="y"
+              autoHide={false}
+              className="custom-scrollbar"
+              style={{ maxHeight: "260px" }}
+            >
+              <div>
+                <div className="recieved msg-chat">
+                  <p>Loream ipsum dolor sldkfj sdklfj </p>
+                  <p>Loream ipsum dolor sldkfj sdklfj </p>
+                </div>
+                <div className="timestamp">
+                  <span>3:16PM</span>
+                </div>
+                <div className="send msg-chat">
+                  <p>Loream ipsum dolor sldkfj sdklfj </p>
+                  <p>Loream ipsum dolor sldkfj sdklfj </p>
+                </div>
+              </div>
+            </SimpleBar>
+            <form className="input-msg-box">
+              <input type="text" />
+              <button className="btn icon-btn">
+                <span className="icon-send"></span>
+              </button>
+            </form>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
